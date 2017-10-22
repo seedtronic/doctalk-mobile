959dd70edfb268900ab5c8166560807c3715f066
marano <thiagomarano@gmail.com>
2017-10-21 22:48:53 -0200

    Google map view.

---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MapView } from 'expo';
+
+export default function Map() {
+  return (
+    <MapView
+      provider={MapView.PROVIDER_GOOGLE}
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  )
+}
