011ccb994e164bf089624b8b07c55b6e1ac0f8cc
marano <thiagomarano@gmail.com>
2017-10-24 19:58:39 -0200

    Remove src folder.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Map from "./src/components/Map"
+import Map from "./components/Map"
 
 export default class App extends React.Component {
   render() {
