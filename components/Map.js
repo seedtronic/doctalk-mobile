544a18b4c2fe4c20c3c30a17303c566700a85ad1
marano <thiagomarano@gmail.com>
2017-10-24 22:46:31 -0200

    Display user current location on load.

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,34 @@
 import React from "react"
 import { MapView } from "expo"
+import { compose, lifecycle, withState } from "recompose"
 
-export default function Map() {
+export default compose(
+  withState("location", "setLocation", null),
+  lifecycle({
+    componentWillMount() {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => {
+          this.props.setLocation({ latitude, longitude })
+        },
+        error => {
+          console.log("Error", error)
+        },
+        {}
+      )
+    }
+  })
+)(Map)
+
+function Map({ location }) {
   return (
     <MapView
       provider={MapView.PROVIDER_GOOGLE}
       style={{ flex: 1 }}
       showsUserLocation={true}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
+      region={{
+        latitude: 0,
+        longitude: 0,
+        ...location,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
       }}
