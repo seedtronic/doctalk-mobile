46bd0a044c5b0ac4e17e6c5cdbc4fcb6d92609c4
marano <thiagomarano@gmail.com>
2017-10-28 19:27:19 -0200

    Configures apollo client.

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
 import React from "react"
-import { Root } from "native-base"
-import { StackNavigator } from "react-navigation"
-import MapScreen from "./screens/MapScreen"
-import SearchScreen from "./screens/SearchScreen"
-import DoctorScreen from "./screens/DoctorScreen"
-
-const MainNavigator = StackNavigator(
-  {
-    MapScreen: { screen: MapScreen },
-    DoctorScreen: { screen: DoctorScreen }
-  },
-  { headerMode: "none" }
-)
-
-const SearchScreenNavigator = StackNavigator(
-  {
-    SearchScreen: { screen: SearchScreen }
-  },
-  { headerMode: "none" }
-)
-
-const RootNavigator = StackNavigator(
-  {
-    MainNavigator: { screen: MainNavigator },
-    SearchScreenNavigator: { screen: SearchScreenNavigator }
-  },
-  { headerMode: "none", mode: "modal" }
-)
+import { ApolloProvider } from "react-apollo"
+import apolloClient from "./lib/apolloClient"
+import Main from "./screens/Main"
 
 export default function App() {
   return (
-    <Root>
-      <RootNavigator />
-    </Root>
+    <ApolloProvider client={apolloClient}>
+      <Main />
+    </ApolloProvider>
   )
 }
---
+++ screens/Main.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Root } from "native-base"
+import { StackNavigator } from "react-navigation"
+import MapScreen from "./MapScreen"
+import SearchScreen from "./SearchScreen"
+import DoctorScreen from "./DoctorScreen"
+
+const MainNavigator = StackNavigator(
+  {
+    MapScreen: { screen: MapScreen },
+    DoctorScreen: { screen: DoctorScreen }
+  },
+  { headerMode: "none" }
+)
+
+const SearchScreenNavigator = StackNavigator(
+  {
+    SearchScreen: { screen: SearchScreen }
+  },
+  { headerMode: "none" }
+)
+
+const RootNavigator = StackNavigator(
+  {
+    MainNavigator: { screen: MainNavigator },
+    SearchScreenNavigator: { screen: SearchScreenNavigator }
+  },
+  { headerMode: "none", mode: "modal" }
+)
+
+export default function App() {
+  return (
+    <Root>
+      <RootNavigator />
+    </Root>
+  )
+}
