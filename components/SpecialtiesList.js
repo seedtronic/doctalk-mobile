bf5da6b389a659016158eb7f7570c2877efad4d0
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2017-11-04 16:30:43 -0200

    Changed the SpecialtiesList items to buttons and also created the component ButtonSpecialtiesList to store the button design information.

--- components/SpecialtiesList.js
+++ components/SpecialtiesList.js
@@ -1,8 +1,10 @@
 import React from "react"
-import { compose, withProps } from "recompose"
+import { compose, withProps} from "recompose"
 import { graphql } from "react-apollo"
 import specialtiesQuery from "../graphql/specialtiesQuery"
-import { List, ListItem, Text } from "native-base"
+import { List, ListItem } from "native-base"
+import ButtonSpecialtiesList from './ButtonSpecialtiesList';
+
 
 export default compose(
   graphql(specialtiesQuery),
@@ -15,13 +17,12 @@ export default compose(
   })
 )(SpecialtiesList)
 
-function SpecialtiesList({ specialties }) {
+function SpecialtiesList({ specialties, navigation }) {
   return <List dataArray={specialties} renderRow={renderRow} />
-
   function renderRow(specialty) {
     return (
       <ListItem>
-        <Text>{specialty.title}</Text>
+        <ButtonSpecialtiesList>{specialty.title}</ButtonSpecialtiesList>
       </ListItem>
     )
   }
