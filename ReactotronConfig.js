f86f048ef93f7b802d0024916a9abf7a5f11a3a1
marano <thiagomarano@gmail.com>
2017-11-05 15:13:24 -0200

    Fix linter and removes unecessary export.

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from "reactotron-react-native"
 import { reactotronRedux } from "reactotron-redux"
 
-export default Reactotron.configure()
+Reactotron.configure()
   .useReactNative()
   .use(reactotronRedux())
   .connect()
