bc2903b4719070e52f29c8a15075f5f4bea506ba
marano <thiagomarano@gmail.com>
2017-11-05 12:08:42 -0200

    Downgrades apollo client and use redux to manage state. Also adds the map filter reducer.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { ApolloProvider } from "react-apollo"
 import apolloClient from "./lib/apolloClient"
+import reduxStore from "./lib/reduxStore"
 import Main from "./screens/Main"
 
 export default function App() {
   return (
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={apolloClient} store={reduxStore}>
       <Main />
     </ApolloProvider>
   )
