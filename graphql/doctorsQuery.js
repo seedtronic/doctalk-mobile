280a9d84a5678e97f0b075ba25b4257e72112671
marano <thiagomarano@gmail.com>
2017-11-05 14:51:25 -0200

    Add specialty id parameter to doctors query.

--- graphql/doctorsQuery.js
+++ graphql/doctorsQuery.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag"
 
 export default gql`
-  query DoctorsQuery {
-    doctors {
+  query DoctorsQuery($specialtyId: ID) {
+    doctors(specialtyId: $specialtyId) {
       edges {
         node {
           id
