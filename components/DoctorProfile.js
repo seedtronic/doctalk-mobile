b13bd556f97f94514692c220a58b8e02b2be8591
marano <thiagomarano@gmail.com>
2017-11-05 22:07:35 -0200

    Doctor profile screen.

--- components/DoctorProfile.js
+++ components/DoctorProfile.js
@@ -1,11 +1,36 @@
 import React from "react"
 import styled from "styled-components/native"
-import { Text } from "native-base"
+import { Button, Text } from "native-base"
 import { branch, compose, renderNothing, withProps } from "recompose"
 import { graphql } from "react-apollo"
 import doctorQuery from "../graphql/doctorQuery"
 
-const Container = styled.View``
+const Container = styled.View`
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+const ImageContainer = styled.View``
+const NameContainer = styled.View`
+  flex-grow: 1;
+  margin-left: 20;
+  margin-top: 20;
+`
+const ButtonContainer = styled.View`
+  align-self: center;
+  padding-left: 10;
+  padding-right: 10;
+  margin-bottom: 10;
+  width: 100%;
+`
+
+const Name = styled.Text`
+  font-size: 24;
+`
+const Image = styled.Image`
+  width: 100%;
+  height: 200;
+`
 
 export default compose(
   graphql(doctorQuery),
@@ -13,10 +38,20 @@ export default compose(
   withProps(({ data: { doctor } }) => ({ doctor }))
 )(DoctorProfile)
 
-function DoctorProfile({ doctor }) {
+function DoctorProfile({ doctor: { name, imageUrl } }) {
   return (
     <Container>
-      <Text>{doctor.name}</Text>
+      <ImageContainer>
+        <Image source={{ uri: imageUrl }} />
+      </ImageContainer>
+      <NameContainer>
+        <Name>{name}</Name>
+      </NameContainer>
+      <ButtonContainer>
+        <Button style={{ width: "100%" }}>
+          <Text>Marcar consulta</Text>
+        </Button>
+      </ButtonContainer>
     </Container>
   )
 }
