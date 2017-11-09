7f68bbbbc3ed69b5a1a5404f38c6df230e9e96c3
marano <thiagomarano@gmail.com>
2017-11-09 19:51:38 -0200

    Allow user to select all specialties.

--- components/SpecialtiesList.js
+++ components/SpecialtiesList.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { View } from "react-native"
 import { compose, withProps } from "recompose"
 import { graphql } from "react-apollo"
 import specialtiesQuery from "../graphql/specialtiesQuery"
@@ -18,11 +19,18 @@ export default compose(
 
 function SpecialtiesList({ specialties, navigation }) {
   return (
-    <List
-      style={{ backgroundColor: "white" }}
-      dataArray={specialties}
-      renderRow={renderRow}
-    />
+    <View>
+      <List style={{ backgroundColor: "white" }}>
+        <SpecialtiesListItem
+          specialty={{ id: null, title: "Todas as especialidades" }}
+        />
+      </List>
+      <List
+        style={{ backgroundColor: "white" }}
+        dataArray={specialties}
+        renderRow={renderRow}
+      />
+    </View>
   )
 }
 
