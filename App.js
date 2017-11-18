b494b194aa4670ed3c53995f23c97fd4f38a4b07
marano <thiagomarano@gmail.com>
2017-11-18 16:23:28 -0200

    Use spinner at persist gate loading.

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import apolloClient from "./lib/apolloClient"
 import reduxStore, { persistor } from "./lib/reduxStore"
 import Navigator from "./screens/Navigator"
 import { PersistGate } from "redux-persist/es/integration/react"
+import SpinnerView from "./components/SpinnerView"
 
 export default function App() {
   return (
     <ApolloProvider client={apolloClient} store={reduxStore}>
-      <PersistGate persistor={persistor}>
+      <PersistGate persistor={persistor} loading={<SpinnerView />}>
         <Navigator />
       </PersistGate>
     </ApolloProvider>
