d6f5edd5d875a9ec2c75e4782900cd5da4cea560
marano <thiagomarano@gmail.com>
2017-11-18 16:19:26 -0200

    Persist application state with redux persist.

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import "./ReactotronConfig.js"
 import React from "react"
 import { ApolloProvider } from "react-apollo"
 import apolloClient from "./lib/apolloClient"
-import reduxStore from "./lib/reduxStore"
+import reduxStore, { persistor } from "./lib/reduxStore"
 import Navigator from "./screens/Navigator"
+import { PersistGate } from "redux-persist/es/integration/react"
 
 export default function App() {
   return (
     <ApolloProvider client={apolloClient} store={reduxStore}>
-      <Navigator />
+      <PersistGate persistor={persistor}>
+        <Navigator />
+      </PersistGate>
     </ApolloProvider>
   )
 }
