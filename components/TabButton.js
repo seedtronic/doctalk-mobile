06df1ec4876dd2d34b7995bb8dc16401deaaba00
marano <thiagomarano@gmail.com>
2017-11-19 15:38:42 -0200

    Extract withIsCurrentRoute HOC.

--- components/TabButton.js
+++ components/TabButton.js
@@ -3,13 +3,11 @@ import { compose } from "recompose"
 import { Button, Text } from "native-base"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import withNavigate from "../lib/withNavigate"
-import withCurrentRoute from "../lib/withCurrentRoute"
-import includes from "lodash/includes"
+import withIsCurrentRoute from "../lib/withIsCurrentRoute"
 
-export default compose(withNavigate, withCurrentRoute)(TabButton)
+export default compose(withNavigate, withIsCurrentRoute)(TabButton)
 
-function TabButton({ routeName, iconName, label, navigate, currentRoutePath }) {
-  const isCurrentRoute = includes(currentRoutePath, routeName)
+function TabButton({ routeName, iconName, label, navigate, isCurrentRoute }) {
   return (
     <Button onPress={navigate(routeName)} active={isCurrentRoute} vertical>
       <MaterialCommunityIcons
