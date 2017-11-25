0b9409f047edfc329bbaf922de9aec178af79676
marano <thiagomarano@gmail.com>
2017-11-25 14:05:31 -0200

    Send user token with requests when authenticated.

--- graphql/loginWithGoogleMutation.js
+++ graphql/loginWithGoogleMutation.js
@@ -4,6 +4,7 @@ export default gql`
   mutation LoginWithGoogle($idToken: String!, $photoUrl: String) {
     loginWithGoogle(idToken: $idToken, photoUrl: $photoUrl) {
       id
+      token
       name
       email
       photoUrl
