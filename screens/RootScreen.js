9de8498b25a148062c6cd100cdd2379cf26bee5b
marano <thiagomarano@gmail.com>
2017-11-30 22:03:32 -0200

    Resets expo on unhandled error.

--- screens/RootScreen.js
+++ screens/RootScreen.js
@@ -3,12 +3,16 @@ import { connect } from "react-redux"
 import { Container, Root } from "native-base"
 import Navigator from "./Navigator"
 import { resetState } from "../lib/reducers"
+import Expo from "expo"
 
 class RootScreen extends Component {
   componentDidCatch(error, info) {
-    console.log(error, info)
-    this.props.resetState()
+    console.log("Unhandled error", error, info)
+    if (Expo.Constants.isDevice) {
+      setTimeout(Expo.Util.reload, 500)
+    }
   }
+
   render() {
     return (
       <Root>
