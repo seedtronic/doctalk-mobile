d0cbc9e9fea4081c56c88054a61719f95fcb255e
marano <thiagomarano@gmail.com>
2017-12-09 12:29:12 -0200

    Do not reset user session on error.

--- components/ClearLocalDataButton.js
+++ components/ClearLocalDataButton.js
@@ -1,15 +1,17 @@
 import { connect } from "react-redux"
 import { compose, withHandlers, withProps } from "recompose"
 import { resetState } from "../lib/reducers"
+import { reset as resetSession } from "../lib/reducers/session"
 import WideButton from "./WideButton"
 import { MaterialIcons } from "@expo/vector-icons"
 import Expo from "expo"
 
 export default compose(
-  connect(null, { resetState }),
+  connect(null, { resetState, resetSession }),
   withHandlers({
-    onPress: ({ resetState }) => () => {
+    onPress: ({ resetState, resetSession }) => () => {
       resetState()
+      resetSession()
       if (Expo.Constants.isDevice) {
         setTimeout(Expo.Util.reload, 500)
       }
