c6bbebd465b84828b4d0adc1581b91e5bc6a74e4
marano <thiagomarano@gmail.com>
2017-12-09 18:24:36 -0200

    Call create appointment mutation.

--- components/AvailableAppointmentScheduleListItem.js
+++ components/AvailableAppointmentScheduleListItem.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { View } from "react-native"
 import styled from "styled-components/native"
 import { ListItem, Text } from "native-base"
 import { DateTime } from "luxon"
--- components/CreateAppointmentButton.js
+++ components/CreateAppointmentButton.js
@@ -1,9 +1,28 @@
 import React from "react"
+import { graphql } from "react-apollo"
+import { compose, withHandlers } from "recompose"
 import { Button, Text } from "native-base"
+import createAppointmentMutation from "../graphql/createAppointmentMutation"
+import withNavigate from "../lib/withNavigate"
 
-export default function CreateAppointmentButton() {
+export default compose(
+  graphql(createAppointmentMutation, {
+    props: ({ mutate, ownProps: { appointmentScheduleId } }) => ({
+      createAppointment: () => mutate({ variables: { appointmentScheduleId } })
+    })
+  }),
+  withNavigate,
+  withHandlers({
+    onPress: ({ createAppointment, goBack, navigate }) => () =>
+      createAppointment()
+        .then(goBack)
+        .then(navigate("DoctorAgendaNavigator"))
+  })
+)(CreateAppointmentButton)
+
+function CreateAppointmentButton({ onPress, appointmentScheduleId }) {
   return (
-    <Button small>
+    <Button small onPress={onPress}>
       <Text>Marcar consulta</Text>
     </Button>
   )
