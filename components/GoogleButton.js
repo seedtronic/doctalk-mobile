77afae48906557c0c22b7838968ec805849794ec
marano <thiagomarano@gmail.com>
2017-12-10 21:41:22 -0200

    Extract google login button behavior.

--- components/GoogleButton.js
+++ components/GoogleButton.js
@@ -1,54 +1,14 @@
-import { connect } from "react-redux"
-import Expo from "expo"
-import { graphql } from "react-apollo"
-import { compose, withHandlers, withProps, withState } from "recompose"
-import loginWithGoogleMutation from "../graphql/loginWithGoogleMutation"
-import { setToken } from "../lib/reducers/session"
+import { compose, withProps } from "recompose"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import WideButton from "./WideButton"
+import withGoogleLogin from "../lib/withGoogleLogin"
 
 export default compose(
-  connect(null, { setToken }),
-  graphql(loginWithGoogleMutation, {
-    props: ({ mutate, ownProps: { setToken } }) => ({
-      loginWithGoogle: variables =>
-        mutate({ variables }).then(response =>
-          setToken(response.data.loginWithGoogle.token)
-        )
-    })
-  }),
-  withState("loading", "setLoading", false),
+  withGoogleLogin,
   withProps(({ loading }) => ({
     iconName: "google",
     IconProvider: MaterialCommunityIcons,
     label: "Entrar com o Google",
     loading
-  })),
-  withHandlers({
-    onPress: props => () => onPress(props)
-  })
+  }))
 )(WideButton)
-
-async function onPress({ loginWithGoogle, setLoading }) {
-  setLoading(true)
-  try {
-    const result = await Expo.Google.logInAsync({
-      androidClientId:
-        "274763169331-n32ea4t1m3kln2asl7gdqv2pu7kaq5ef.apps.googleusercontent.com",
-      iosClientId:
-        "274763169331-mc3r4b0e79i72a576ebtj2agrd26fot1.apps.googleusercontent.com",
-      webClientId:
-        "274763169331-frpn69903v61iu42m1kdgs8iq2d8383s.apps.googleusercontent.com",
-      scopes: ["profile", "email"]
-    })
-    if (result.type === "success") {
-      loginWithGoogle({
-        idToken: result.idToken,
-        photoUrl: result.user.photoUrl
-      })
-    }
-  } catch (error) {
-    console.log(error)
-  }
-  setLoading(false)
-}
